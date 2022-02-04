1717934f229d8dbe07e24bfa62d119dcff4cce4c
Rakib Hasan Babu <rhbabu3@gmail.com>
2022-02-04 20:55:33 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const { MongoClient } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.whfic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+console.log(uri);
+
+app.get('/', (req, res) => {
+    res.send('Smart Shop server is running ...');
+});
+
+app.listen(port, (req, res) => {
+    console.log('Smart Shop server is listning at port', port);
+});
